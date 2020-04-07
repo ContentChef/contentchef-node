94aa0ebd04230e8b10c8b00c97718e8aa9d2705f
PierpaoloIannone <piannone@byte-code.com>
2020-04-07 10:43:24 +0200

    feat: add our cloudName as default

--- src/services/Media/index.ts
+++ src/services/Media/index.ts
@@ -1,18 +1,19 @@
 import * as cloudinary from 'cloudinary-core';
 
-const client = cloudinary.Cloudinary.new({});
-
 export type IImageTag = cloudinary.ImageTag;
 export type IVideoTag = cloudinary.VideoTag;
 
-export function createUrl(publicId: string, cloudName: string): string {
+const defaultCloudName = 'contentchef';
+const client = cloudinary.Cloudinary.new({cloud_name: defaultCloudName});
+
+export function createUrl(publicId: string, cloudName: string = defaultCloudName): string {
     return client.url(publicId, { cloud_name: cloudName });
 }
 
-export function createImageTag(publicId: string, cloudName: string): IImageTag {
+export function createImageTag(publicId: string, cloudName: string = defaultCloudName): IImageTag {
     return client.imageTag(publicId, { cloud_name: cloudName });
 }
 
-export function createVideoTag(publicId: string, cloudName: string): IVideoTag {
+export function createVideoTag(publicId: string, cloudName: string = defaultCloudName): IVideoTag {
     return client.videoTag(publicId, { cloud_name: cloudName });
 }
