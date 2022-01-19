fc153891e11a97d5298972ffb3f0bbca1086d0cd
PierpaoloIannone <piannone@byte-code.com>
2022-01-19 12:23:42 +0100

    fix: export types from cloudinary-build-url

--- packages/contentchef-media/src/index.ts
+++ packages/contentchef-media/src/index.ts
@@ -1,7 +1,8 @@
-import { CldOptions } from '@cld-apis/types';
+import { TransformerOption, TransformerVideoOption } from '@cld-apis/types';
 import buildUrl from 'cloudinary-build-url';
 
-export type IMediaOptions = CldOptions['transformations'] & {cloud_name?: string};
+export type IMediaOptions = (TransformerOption | TransformerVideoOption) & {cloud_name?: string};
+export type IVideoOptions = TransformerVideoOption & {cloud_name?: string};
 
 export enum ResourceType {
     image = 'image',
@@ -20,10 +21,25 @@ export function imageUrl(publicId: string, options?: IMediaOptions) {
     return createUrl(publicId, options, ResourceType.image);
 }
 
-export function videoUrl(publicId: string, options?: IMediaOptions) {
+export function videoUrl(publicId: string, options?: IVideoOptions) {
     return createUrl(publicId, options, ResourceType.video);
 }
 
 export function rawFileUrl(publicId: string, options?: IMediaOptions) {
     return createUrl(publicId, options, ResourceType.raw);
 }
+
+export {
+    AudioCodec, Border, ColorSpace, CompassGravity, Condition,
+    ConditionExpression, CustomFunction, Effect, Expression,
+    Flag, FPS, FPSType, Gravity,
+    Offset,
+    Position,
+    Radius,
+    Resize,
+    ResizeType,
+    Rotation, StringValue, TextStyle, Transformation, TransformerBaseOptions,
+    TransformerOption,
+    TransformerVideoOption, Variable, VColorSpace, VEffect, VFlag
+} from '@cld-apis/types';
+
